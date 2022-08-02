75aa0d551f81b1a643f3f989a1d1ac481c76bce1
jvarc <80175223+jvarc@users.noreply.github.com>
2022-08-02 15:33:04 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiKey: 'pk.eyJ1IjoianZhcmMiLCJhIjoiY2w1emE3MTEyMWI5ejNlbnFjZm5jcWEwZSJ9.Cc1w9iCkSq1FGP7Qk3RDUw'
+  apiKey: 'aqui va tu apiKey'
 };
