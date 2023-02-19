3edf192aca342d8506d93b92d9572f11d330d18e
jvarc <80175223+jvarc@users.noreply.github.com>
2023-02-19 12:17:52 +0000

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  API_KEY: 'pk.eyJ1IjoianZhcmMiLCJhIjoiY2xlOGRjZ3VsMGVhODN2bmJ3cmZraDltOCJ9._ifIhQAwg2xj_VHulxH0cg'
+  API_KEY: 'aqui tu apikey de mapbox'
 };
